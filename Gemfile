872b0cc287f2a4a9cfb20c0bf0ee0594b4713c2e
virus <virusbb001a@gmail.com>
2014-10-24 18:36:49 +0900

    Finish Chapter3 exercise

--- Gemfile
+++ Gemfile
@@ -3,11 +3,17 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
+# use postagleSQL
+gem 'pg'
 
 group :development, :test do
  # Use sqlite3 as the database for Active Record
- gem 'sqlite3', '1.3.8'
+ #gem 'sqlite3', '1.3.8'
+ gem 'spork-rails', '4.0.0'
  gem 'rspec-rails', '2.13.1'
+ gem 'guard-rspec', '2.5.0'
+ gem 'guard-spork', '1.5.0'
+ gem 'childprocess', '0.3.6'
 end
 
 group :test do
@@ -37,6 +43,6 @@ group :doc do
 end
 
 group :production do
- gem 'pg','0.15.1'
+ # gem 'pg','0.15.1'
  gem 'rails_12factor', '0.0.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+ let (:base_title){ "Ruby on Rails Tutorial Sample App" }
  describe "Home page" do
   
   it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
 
   it "should have the title 'Home'" do
    visit '/static_pages/home'
-   expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+   expect(page).to have_title("#{base_title} | Home")
   end
  end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
 
   it "should have the title 'Help'" do
    visit '/static_pages/help'
-   expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+   expect(page).to have_title("#{base_title} | Help")
   end
  end
 
@@ -36,7 +38,14 @@ describe "Static pages" do
 
   it "should have the title 'About Us'" do
    visit '/static_pages/about'
-   expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+   expect(page).to have_title("#{base_title} | About Us")
+  end
+ end
+
+ describe "Contact" do
+  it "should have the title 'Contact'" do
+   visit '/static_pages/contact'
+   expect(page).to have_title("#{base_title} | Contact")
   end
  end
 end
