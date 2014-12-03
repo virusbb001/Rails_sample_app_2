91ec4d464f75f3c5cff0ed71ed9d34f89973f52f
virus <virusbb001a@gmail.com>
2014-12-04 07:29:50 +0900

    9.2.3 Complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,6 +47,19 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          valid_signin(user)
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
